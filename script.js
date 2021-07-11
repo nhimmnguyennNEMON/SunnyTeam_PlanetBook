31f692593bad276e404b0e0f83bebcff13753346
SamSam16 <78789558+nguyenvansy16@users.noreply.github.com>
2021-07-11 14:05:07 +0700

    fix file index & main & script

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ showBtn.addEventListener('click', function(){
 
 /* Lightbox */
 var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
+
+
+sr.reveal(`.about-content, .blockquote, .home__info,
+           .discover__container,
+           .experience__data, .experience__overlay,
+           .place__card,
+           .sponsor__content,
+           .footer__data, .footer__rights`,{
+    origin: 'top',
+    interval: 100,
+})
